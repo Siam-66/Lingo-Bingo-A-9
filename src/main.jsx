70d13390b3bcefaaf49fb708d2a87d97394cb3e3
Siam-66 <siammahmud66@gmail.com>
2024-11-21 19:58:02 +0600

    adding Json

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Component/Home';
+import Root from './Component/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    
+    children: [
+      {
+        path: "/",
+        element: <Home></Home> ,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <RouterProvider router={router} />
+  </StrictMode>,
+)
